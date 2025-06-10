60cc8dd3f10ba045dc680dd32f1bd8504c092d1c
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-06-10 16:11:46 +0530

    Refactor Shelf model to include HasFactory trait and reorder fillable properties for consistency.

--- app/Models/Shelf.php
+++ app/Models/Shelf.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shelf extends Model
 {
+    use HasFactory;
+
     protected $table = 'shelf';
-    protected $fillable = ['name', 'user_id'];
+    protected $fillable = ['user_id', 'name'];
 
     /**
      * Get the user that owns the shelf.
